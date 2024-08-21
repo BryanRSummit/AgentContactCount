4a845e6e4afffe2c18d3da0d3e272bb11a087f59
BryanRSummit <bryan@reddsummit.com>
2024-08-21 10:29:23 -0600

    changing service accounts

--- sheets_login.py
+++ sheets_login.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 def sheets_login():
     # Google Sheets credentials
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive', "https://www.googleapis.com/auth/gmail.send"]
-    gmailCreds = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\Bryan Edman\Documents\Count Contact Tool\agentleadmailer-8cc577104ac3.json', scope)
+    gmailCreds = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\Bryan Edman\Documents\Count Contact Tool\agentcontactcount-01c64e5317e2.json', scope)
     gc = gspread.authorize(gmailCreds)
 
     # Open the Google Sheet
